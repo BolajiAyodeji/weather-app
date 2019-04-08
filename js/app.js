f6cb615f9b093e0742d9f26913ca764ecd52cd65
Bolaji Ayodeji <sirbeejay1@gmail.com>
2019-04-08 15:13:24 +0100

    setup change location

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@ const weather = new Weather('Lokoja', 'Nigeria');
 
 document.addEventListener('DOMContentLoaded', getWeather)
 
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+  const city = document.getElementById('city').value;
+  const country = document.getElementById('country').value;
+
+  if(city === '' || country === '') {
+    alert('Incomplete fields');
+  } else {
+    weather.changeLocation(city, country);
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+  }
+});
+
 function getWeather() {
   weather.getWeather()
     .then(res => {
